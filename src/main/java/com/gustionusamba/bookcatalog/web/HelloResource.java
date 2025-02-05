485ac1dfcd058463cbc8667d6a762ac7ffa9b2dc
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-02-05 14:48:58 +0700

    feat: create custom log

--- src/main/java/com/gustionusamba/bookcatalog/web/HelloResource.java
+++ src/main/java/com/gustionusamba/bookcatalog/web/HelloResource.java
@@ -1,11 +1,15 @@
 package com.gustionusamba.bookcatalog.web;
 
 import com.gustionusamba.bookcatalog.service.GreetingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
+
+    Logger log = LoggerFactory.getLogger(HelloResource.class);
 
     private final GreetingService greetingService;
 
@@ -16,6 +20,12 @@ public class HelloResource {
 
     @GetMapping("/hello")
     public String helloWorld() {
+        log.error("This is error message");
+        log.warn("This is warn message");
+        log.info("This is info message");
+        log.debug("This is debug message");
+        log.trace("This is trace message");
+
         return greetingService.sayGreeting();
     }
 }
