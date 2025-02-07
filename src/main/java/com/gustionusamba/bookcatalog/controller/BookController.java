9dc84e2639d8205e5c7e4759121aaeb8fd06d5fe
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-02-07 18:13:00 +0700

    feat: implement a validation

--- src/main/java/com/gustionusamba/bookcatalog/controller/BookController.java
+++ src/main/java/com/gustionusamba/bookcatalog/controller/BookController.java
@@ -3,9 +3,12 @@ package com.gustionusamba.bookcatalog.controller;
 import com.gustionusamba.bookcatalog.dto.BookCreateDTO;
 import com.gustionusamba.bookcatalog.dto.BookDetailDTO;
 import com.gustionusamba.bookcatalog.service.BookService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,7 +38,11 @@ public class BookController {
     }
 
     @PostMapping("/new")
-    public String addNewBook(@ModelAttribute("bookCreateDTO") BookCreateDTO dto, Model model) {
+    public String addNewBook(@ModelAttribute("bookCreateDTO") @Valid BookCreateDTO dto, BindingResult bindingResult, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("bookCreateDTO", dto);
+            return "book/book-new";
+        }
         bookService.createNewBook(dto);
         return "redirect:/book/list";
     }
--- src/main/java/com/gustionusamba/bookcatalog/dto/BookCreateDTO.java
+++ src/main/java/com/gustionusamba/bookcatalog/dto/BookCreateDTO.java
@@ -1,13 +1,16 @@
 package com.gustionusamba.bookcatalog.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class BookCreateDTO {
 
+    @NotBlank
     private String bookTitle;
 
     private String bookDescription;
 
+    @NotBlank
     private String authorName;
 }
