f258dc370683e1b11d138b20240bfcd7aeba2c21
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-02-25 10:50:57 +0700

    feat: set datasource-proxy for logging sql query activities

--- src/main/java/com/gustionusamba/bookcatalog/config/DataSourceProxyBeanProcessor.java
+++ src/main/java/com/gustionusamba/bookcatalog/config/DataSourceProxyBeanProcessor.java
@@ -1,11 +1,18 @@
 package com.gustionusamba.bookcatalog.config;
 
+import java.lang.reflect.Method;
+
 import javax.sql.DataSource;
 
 import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
+
+import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
 
 public class DataSourceProxyBeanProcessor implements BeanPostProcessor {
 
@@ -19,7 +26,10 @@ public class DataSourceProxyBeanProcessor implements BeanPostProcessor {
         if (bean instanceof DataSource) {
             ProxyFactory factory = new ProxyFactory(bean);
             factory.setProxyTargetClass(true);
+            factory.addAdvice(new ProxyDataSourceInterceptor((DataSource) bean));
+            return factory.getProxy();
         }
+        return bean;
     }
 
     private static class ProxyDataSourceInterceptor implements MethodInterceptor {
@@ -27,9 +37,17 @@ public class DataSourceProxyBeanProcessor implements BeanPostProcessor {
 
         public ProxyDataSourceInterceptor(final DataSource dataSource) {
             super();
-            this.dataSource = 
+            this.dataSource = ProxyDataSourceBuilder.create(dataSource).countQuery().logQueryBySlf4j(SLF4JLogLevel.INFO)
+                    .build();
         }
 
-        
+        @Override
+        public Object invoke(final MethodInvocation invocation) throws Throwable {
+            Method proxyMethod = ReflectionUtils.findMethod(dataSource.getClass(), invocation.getMethod().getName());
+            if (proxyMethod != null) {
+                return proxyMethod.invoke(dataSource, invocation.getArguments());
+            }
+            return invocation.proceed();
+        }
     }
 }
