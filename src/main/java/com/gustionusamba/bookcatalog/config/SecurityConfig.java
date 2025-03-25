4ec096af70feffa37293a3fa15fc27f029015fd8
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-03-25 16:55:47 +0700

    feat: implement authorization based on basic auth

--- src/main/java/com/gustionusamba/bookcatalog/config/SecurityConfig.java
+++ src/main/java/com/gustionusamba/bookcatalog/config/SecurityConfig.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,14 +21,15 @@ public class SecurityConfig {
     @Autowired
     private AppUserService appUserService;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Autowired
+    void registerProvider(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
+        auth.userDetailsService(appUserService).passwordEncoder(passwordEncoder);
     }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((authz) ->
+        return http
+                .authorizeHttpRequests((authz) ->
                         authz.anyRequest().authenticated())
                 .csrf((csrf) -> csrf.disable())
                 .sessionManagement(sessionManagementCustomizer -> sessionManagementCustomizer
