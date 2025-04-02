cdbd127ea05a918130a231aeb32560ef3ca5f044
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-04-02 12:35:36 +0700

    feat: customize validation on argument level

--- src/main/java/com/gustionusamba/bookcatalog/web/PublisherResource.java
+++ src/main/java/com/gustionusamba/bookcatalog/web/PublisherResource.java
@@ -7,29 +7,33 @@ import com.gustionusamba.bookcatalog.dto.PublisherUpdateDTO;
 import com.gustionusamba.bookcatalog.dto.ResultPageResponseDTO;
 import com.gustionusamba.bookcatalog.service.PublisherService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 @RestController
 @AllArgsConstructor
+@Validated
 public class PublisherResource {
 
     private final PublisherService publisherService;
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/v1/publisher")
-    public ResponseEntity<Void> createANewPublisher(@RequestBody @Valid PublisherCreateDTO dto) {
+    public ResponseEntity<Void> updatePublisher(@RequestBody @Valid PublisherCreateDTO dto) {
         publisherService.createPublisher(dto);
         return ResponseEntity.created(URI.create("/v1/publisher")).build();
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/v1/publisher/{publisherId}")
-    public ResponseEntity<Void> createANewPublisher(@PathVariable("publisherId") String publisherId, @RequestBody @Valid PublisherUpdateDTO dto) {
+    public ResponseEntity<Void> updatePublisher(@PathVariable("publisherId") @Size(max = 36, min = 36, message = "publisher.id.not.uuid") String publisherId,
+                                                @RequestBody @Valid PublisherUpdateDTO dto) {
         publisherService.updatePublisher(publisherId, dto);
         return ResponseEntity.ok().build();
     }
